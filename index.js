c7fee2b6cf8db5d3b0f257574950df7cfab6f83d
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-15 00:27:34 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+const findMatching = function(arr,str){
+ return arr.filter((el) => el.toLowerCase().includes(str.toLowerCase()))
+}
+
+const fuzzyMatch = function(arr,str){
+  return  arr.filter(function firstN(arr) {
+    return arr.indexOf(str) === 0;
+  })
+}
+
+const matchName = function(arr,str) {
+    return  arr.filter(function firstN(arr) {
+        return arr.name === str;
+      })
+    }
+    
